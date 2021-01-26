4a5289de7a81e538dd90fb147c349b79f428ef6c
Grant Wasil <dev@grantwasil.com>
2021-01-25 23:52:43 -0700

    feat: character page displays character data

--- client/src/components/Character/Character.js
+++ client/src/components/Character/Character.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Heading } from 'grommet';
+import { Box, Heading, Paragraph, Text } from 'grommet';
 
 function Character(props) {
   const { characterData } = props;
@@ -7,7 +7,11 @@ function Character(props) {
   console.log(characterData);
   return (
     <Box>
-      <Heading>Your character</Heading>
+      <Heading>{characterData.name}</Heading>
+      <Text>`How people see you: {characterData.traits}</Text>
+      <Paragraph>`Some information to help you prepare: {characterData.preview}</Paragraph>
+
+      <Text>You will recieve more information about your character on the day of the event. Feel free to ask Grant if you have any questions</Text>
     </Box>
   )
 }
