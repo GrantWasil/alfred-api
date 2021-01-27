6a98306d6786e34b2bb56a1e5e6bfd93fe798cc9
Grant Wasil <dev@grantwasil.com>
2021-01-27 03:21:23 -0700

    fix: updated the character page

--- client/src/components/Character/Character.js
+++ client/src/components/Character/Character.js
@@ -18,13 +18,10 @@ function Character(props) {
       <Paragraph textAlign="center">
         Some information to help you prepare: {characterData.preview}
       </Paragraph>
-      <Heading level="2" textAlign="center">
-        Grant will be posting more imporant information here in the next two
-        days. Check back.
-      </Heading>
       <Text>
-        You will recieve more information about your character on the day of the
-        event. Feel free to ask Grant if you have any questions
+        This character information is only for you! You will recieve more
+        information about your character on the day of the event. Feel free to
+        ask Grant if you have any questions
       </Text>
     </Box>
   );
