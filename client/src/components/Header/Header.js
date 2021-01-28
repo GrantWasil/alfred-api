58265406d579c19f66ef4928d07490defde43174
Grant Wasil <dev@grantwasil.com>
2021-01-28 04:43:22 -0700

    fix: updated Header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,35 +1,87 @@
-import React, { useState } from 'react';
-import { Link } from '@reach/router';
-import { Flex, Box, Icon, Button, Text, Heading } from '@chakra-ui/react';
-import { CloseIcon, HamburgerIcon } from '@chakra-ui/icons';
-import './Header.css';
+import React from 'react';
+import { Link, Box, Flex, Text, Button, Stack } from '@chakra-ui/react';
+import { Link as ReachLink } from '@reach/router';
 
-const HeaderIcon = (props) => (
-  <svg viewBox="0 0 100 100" {...props}>
-    <path d="M16.667,43.33C16.667,69.104,37.559,90,63.333,90v-6.667c-22.093,0-40-17.91-40-40.003H16.667z"></path>
-    <path d="M63.333,16.667c-14.729,0-26.666,11.94-26.666,26.664C36.667,58.06,48.604,70,63.333,70v6.667  C44.922,76.667,30,61.741,30,43.33C30,24.922,44.922,10,63.333,10V16.667z"></path>
-    <path d="M63.333,43.333v20c-11.048,0-20-8.955-20-20.003c0-11.045,8.952-19.997,20-19.997c11.049,0,20,8.952,20,19.997  L63.333,43.333z"></path>
-  </svg>
-);
+import Logo from '../Logo/Logo';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
 
-const MenuItems = (props) => {
-  const { children, isLast, to = '/', ...rest } = props;
+const Header = (props) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const { characterData, onLogoutCharacter } = props;
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
-    <Text
-      mb={{ base: isLast ? 0 : 8, sm: 0 }}
-      mr={{ base: 0, sm: isLast ? 0 : 8 }}
-      display="block"
-      {...rest}
-    >
-      <Link to={to}>{children}</Link>
-    </Text>
+    <NavBarContainer {...props}>
+      <Logo
+        w="100px"
+        color={['white', 'white', 'primary.500', 'primary.500']}
+      />
+      <MenuToggle toggle={toggle} isOpen={isOpen} />
+      <MenuLinks isOpen={isOpen} characterData={characterData} onLogoutCharacter={onLogoutCharacter} />
+    </NavBarContainer>
   );
 };
 
-function Header(props) {
-  const [ show, setShow ] = useState(false);
-  const { characterData, onLogoutCharacter, socketStatus } = props;
-  const toggleMenu = () => setShow(!show);
+const MenuToggle = ({ toggle, isOpen }) => {
+  return (
+    <Box display={{ base: 'block', md: 'none' }} onClick={toggle}>
+      {isOpen ? <CloseIcon /> : <HamburgerIcon />}
+    </Box>
+  );
+};
+
+const MenuItem = ({ children, isLast, to = '/', ...rest }) => {
+  return (
+    <Link as={ReachLink} to={to}>
+      <Text display="block" {...rest}>
+        {children}
+      </Text>
+    </Link>
+  );
+};
+
+const MenuLinks = (props) => {
+  const { isOpen, characterData, onLogoutCharacter } = props;
+  return (
+    <Box
+      display={{ base: isOpen ? 'block' : 'none', md: 'block' }}
+      flexBasis={{ base: '100%', md: 'auto' }}
+    >
+      <Stack
+        spacing={8}
+        align="center"
+        justify={['center', 'space-between', 'flex-end', 'flex-end']}
+        direction={['column', 'row', 'row', 'row']}
+        pt={[4, 4, 0, 0]}
+      >
+        <MenuItem to="/me">Your Character</MenuItem>
+        <MenuItem to="/event">Event Info</MenuItem>
+        <MenuItem to="/list">Guest List </MenuItem>
+        {characterData.keyword === 'testing' ? (
+          <MenuItem to="/create">Create </MenuItem>
+        ) : (
+          <> </>
+        )}
+        <MenuItem to="/login" isLast>
+          <Button
+            onClick={onLogoutCharacter}
+            size="sm"
+            rounded="md"
+            color={['primary.500', 'primary.500', 'white', 'white']}
+            bg={['white', 'white', 'primary.500', 'primary.500']}
+            _hover={{
+              bg: ['primary.100', 'primary.100', 'primary.600', 'primary.600'],
+            }}
+          >
+            Logout
+          </Button>
+        </MenuItem>
+      </Stack>
+    </Box>
+  );
+};
+
+const NavBarContainer = ({ children, ...props }) => {
   return (
     <Flex
       as="nav"
@@ -43,60 +95,9 @@ function Header(props) {
       color={['white', 'white', 'primary.700', 'primary.700']}
       {...props}
     >
-      <Flex align="center">
-        <Icon as={HeaderIcon} />
-        <Heading as="h3" size="lg">Alfred</Heading>
-      </Flex>
-      <Box display={{ base: "block", md: "none" }} onClick={toggleMenu}>
-        {show ? <CloseIcon /> : <HamburgerIcon />}
-      </Box>
-      <Box
-        display={{ base: show ? 'block' : 'none', md: 'block' }}
-        flexBasis={{ base: '100%', md: 'auto' }}
-      ></Box>
-      {characterData.name ? (
-        <Flex
-          align={['center', 'center', 'center', 'center']}
-          justify={['center', 'space-between', 'flex-end', 'flex-end']}
-          direction={['column', 'row', 'row', 'row']}
-          pt={[4, 4, 0, 0]}
-        >
-          {socketStatus ? <p>Online</p> : <p>Offline</p>}
-          <MenuItems to="/me">Your Character</MenuItems>
-          <MenuItems to="/event">Event Info </MenuItems>
-          <MenuItems to="/list">Guest List </MenuItems>
-          {characterData.keyword === 'testing' ? (
-            <MenuItems className="header__link" to="/create">
-              Create
-            </MenuItems>
-          ) : (
-            <></>
-          )}
-          <MenuItems to="/signup" isLast>
-            <Button
-              onClick={onLogoutCharacter}
-              size="sm"
-              rounded="md"
-              color={['primary.500', 'primary.500', 'white', 'white']}
-              bg={['white', 'white', 'primary.500', 'primary.500']}
-              _hover={{
-                bg: [
-                  'primary.100',
-                  'primary.100',
-                  'primary.600',
-                  'primary.600',
-                ],
-              }}
-            >
-              Create Account
-            </Button>
-          </MenuItems>
-        </Flex>
-      ) : (
-        <> </>
-      )}
+      {children}
     </Flex>
   );
-}
+};
 
 export default Header;
---
+++ client/src/components/Logo/Logo.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Box, Heading, Icon } from '@chakra-ui/react';
+
+const HeaderIcon = (props) => (
+  <svg viewBox="0 0 100 100" {...props}>
+    <path d="M16.667,43.33C16.667,69.104,37.559,90,63.333,90v-6.667c-22.093,0-40-17.91-40-40.003H16.667z"></path>
+    <path d="M63.333,16.667c-14.729,0-26.666,11.94-26.666,26.664C36.667,58.06,48.604,70,63.333,70v6.667  C44.922,76.667,30,61.741,30,43.33C30,24.922,44.922,10,63.333,10V16.667z"></path>
+    <path d="M63.333,43.333v20c-11.048,0-20-8.955-20-20.003c0-11.045,8.952-19.997,20-19.997c11.049,0,20,8.952,20,19.997  L63.333,43.333z"></path>
+  </svg>
+);
+
+function Logo(props) {
+  return (
+    <Box {...props}>
+      <Icon as={HeaderIcon} h={36} w={36} />
+      <Heading size="lg">Alfred</Heading>
+    </Box>
+  );
+}
+
+export default Logo;
