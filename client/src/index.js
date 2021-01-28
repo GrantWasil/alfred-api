345d62a50a8af9be13bca16d083d9e9958294436
Grant Wasil <dev@grantwasil.com>
2021-01-28 03:20:06 -0700

    feat: added ColorModeScript to app

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import { ColorModeScript } from '@chakra-ui/react';
 
 ReactDOM.render(
   <React.StrictMode>
+    <ColorModeScript initialColorMode={'system'} />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
