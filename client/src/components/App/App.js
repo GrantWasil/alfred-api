ca24ff7d0a6ad2551395fcf1632752b0bfefeb3e
Grant Wasil <dev@grantwasil.com>
2021-01-28 03:17:42 -0700

    feat: starting to remove grommit in exchange for chakra-ui

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,29 +1,12 @@
 import React from 'react';
-import { Grommet } from 'grommet';
 import Main from '../Main/Main';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function App() {
-  const theme = {
-    global: {
-      colors: {
-        brand: '#264653',
-        focus: '#a8dadc',
-        'accent-1': '#2a9d8f',
-        'accent-2': '#e9c46a',
-        'accent-3': '#f4a261',
-        'accent-4': '#e76f51',
-      },
-    },
-    font: {
-      family: 'Halant',
-      size: '18px',
-      height: '20px',
-    },
-  };
   return (
-    <Grommet theme={theme} full>
+    <ChakraProvider>
       <Main />
-    </Grommet>
+    </ChakraProvider>
   );
 }
 
