7f8604c7c3b87d7dc2755e409a450059ae7e71b8
Grant Wasil <dev@grantwasil.com>
2021-01-29 17:50:22 -0700

    fix: eventInfo now uses charkaui

--- client/src/components/EventInfo/EventInfo.js
+++ client/src/components/EventInfo/EventInfo.js
@@ -1,40 +1,37 @@
 import React from 'react';
-import { Paragraph, Heading } from 'grommet';
-import { Box } from '@chakra-ui/react';
+import { Flex, Heading, Text } from '@chakra-ui/react';
 
 function EventInfo() {
   return (
-    <Box
-      align="center"
-      justify="center"
-      flex
-      pad={'small'}
+    <Flex
+      justifyContent="center"
+      alignContent="center"
       textAlign="center"
     >
       <Heading>Casino Fatale</Heading>
-      <Heading level="3" alignSelf="start">
+      <Heading as="h2" size="2xl" alignSelf="start">
         The Setting
       </Heading>
-      <Paragraph>
+      <Text as="p">
         The banks of the River Seine in Paris; a dark night, and glittering
         casino lights. The wealthy of Paris, the talented, the brilliant... and
         a few people who are frankly just hangers-on... are descending on the
         Hotel D’Etoile to take part in a charity auction for the local
         orphanage, and you are about to join them!
-      </Paragraph>
-      <Paragraph>
+      </Text>
+      <Text as="p">
         Money will change hands, reputations will be made and lost, and intrigue
         will thicken, as wealth, crime and politics come together to play...
         Welcome to Casino Fatale!
-      </Paragraph>
-      <Paragraph>
+      </Text>
+      <Text as="p">
         “You are warmly invited to come and spend lots of money in the
         name of charity at Hotel D’Etoile. We are holding a charity auction for
         our local orphanage and will be auctioning jewellery and the services of
         several local characters. Any further donations please contact the
         casino before the event.”
-      </Paragraph>
-    </Box>
+      </Text>
+    </Flex>
   );
 }
 
