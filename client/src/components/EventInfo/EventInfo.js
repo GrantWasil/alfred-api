b130aa7f835c5138f1f4cdd5554fbcce83d4787e
Grant Wasil <dev@grantwasil.com>
2021-01-29 19:02:42 -0700

    fix: testing my

--- client/src/components/EventInfo/EventInfo.js
+++ client/src/components/EventInfo/EventInfo.js
@@ -8,22 +8,23 @@ function EventInfo() {
       alignContent="space-between"
       textAlign="center"
       flexDirection="column"
-      minH="70vh"
+      h="100%"
     >
-      <Heading>Casino Fatale Setting</Heading>
-      <Text as="p">
+      <Heading as="h1">Casino Fatale</Heading>
+      <Heading as="h2" alignSelf="start">The setting</Heading>
+      <Text as="p" my={1}>
         The banks of the River Seine in Paris; a dark night, and glittering
         casino lights. The wealthy of Paris, the talented, the brilliant... and
         a few people who are frankly just hangers-on... are descending on the
         Hotel D’Etoile to take part in a charity auction for the local
         orphanage, and you are about to join them!
       </Text>
-      <Text as="p">
+      <Text as="p" my={1}>
         Money will change hands, reputations will be made and lost, and intrigue
         will thicken, as wealth, crime and politics come together to play...
         Welcome to Casino Fatale!
       </Text>
-      <Text as="p">
+      <Text as="p" my={1}>
         “You are warmly invited to come and spend lots of money in the
         name of charity at Hotel D’Etoile. We are holding a charity auction for
         our local orphanage and will be auctioning jewellery and the services of
