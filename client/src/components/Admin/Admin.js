f0e602944c1510d5d3614c6bf935801f36088f7f
Grant Wasil <dev@grantwasil.com>
2021-01-31 23:55:56 -0700

    fix: changed button colors and removed e.prev.default

--- client/src/components/Admin/Admin.js
+++ client/src/components/Admin/Admin.js
@@ -4,26 +4,23 @@ import { Stack, Button } from '@chakra-ui/react';
 function Admin(props) {
   const { onUpdateGamemode } = props;
 
-  function onStageOneButton(e) {
-    e.prevent.default();
+  function onStageOneButton() {
     onUpdateGamemode(1);
   }
 
-  function onStageTwoButton(e) {
-    e.prevent.default();
+  function onStageTwoButton() {
     onUpdateGamemode(2);
   }
 
-  function onStageThreeButton(e) {
-    e.prevent.default();
+  function onStageThreeButton() {
     onUpdateGamemode(3);
   }
 
   return (
     <Stack spacing={3}>
-      <Button colorScheme="green 100" onClick={onStageOneButton} >Stage 1</Button>
-      <Button colorScheme="blue 100" onClick={onStageTwoButton} >Stage 2</Button>
-      <Button colorScheme="purple 100" onClick={onStageThreeButton} >Stage 3</Button>
+      <Button colorScheme="green" onClick={onStageOneButton} >Stage 1</Button>
+      <Button colorScheme="blue" onClick={onStageTwoButton} >Stage 2</Button>
+      <Button colorScheme="purple" onClick={onStageThreeButton} >Stage 3</Button>
     </Stack>
   )
 }
