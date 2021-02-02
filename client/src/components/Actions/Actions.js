10df0c204e5bb280ca99aff7608f2a2cfc50cb9e
Grant Wasil <dev@grantwasil.com>
2021-02-02 15:14:57 -0700

    feat: added secret and clue to actions

--- client/src/components/Actions/Actions.js
+++ client/src/components/Actions/Actions.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Stack, Flex, Button, Grid, GridItem } from '@chakra-ui/react';
+import { Stack, Button, Grid, GridItem, Heading, Text } from '@chakra-ui/react';
 
 function Actions(props) {
   const { setSearch, characterData } = props;
@@ -16,7 +16,15 @@ function Actions(props) {
       templateColumns="repeat(3, 1fr)"
       gap={3}
     >
-      <GridItem rowSpan={1} colSpan={3} bg="tomato" />
+      <GridItem rowSpan={1} colSpan={3} >
+        <Stack justify="center">
+          <Heading>{characterData.name}</Heading>
+          <Stack direction={"column"} spacing={2}>
+            <Text>Secret: {characterData.secret}</Text>
+            <Text>Clue: {characterData.clue}</Text>
+          </Stack>
+        </Stack>
+      </GridItem>
       <GridItem rowSpan={1} colSpan={1} bg="white" />
       <GridItem rowSpan={3} colSpan={2} bg="tomato" />
       <GridItem rowSpan={1} colSpan={1} bg="white" />
