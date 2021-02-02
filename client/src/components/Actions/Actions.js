36b31ecbc8a33efcdc80758f126decbe5c760aa9
Grant Wasil <dev@grantwasil.com>
2021-02-02 16:59:16 -0700

    fix: actions should only render with data

--- client/src/components/Actions/Actions.js
+++ client/src/components/Actions/Actions.js
@@ -9,40 +9,48 @@ function Actions(props) {
   }
 
   return (
-    <Grid
-      h="80vh"
-      w="80vw"
-      templateRows="repeat(5, 1fr)"
-      templateColumns="repeat(3, 1fr)"
-      gap={3}
-    >
-      <GridItem rowSpan={1} colSpan={3}>
-        <Stack textAlign="center">
-          <Heading>{characterData.name}</Heading>
-          <Stack direction={'row'} spacing={2} justify="space-between">
-            <Text>Secret: {characterData.secret}</Text>
-            <Text>Clue: {characterData.clue}</Text>
-          </Stack>
-        </Stack>
-      </GridItem>
-      <GridItem rowSpan={1} colSpan={1}>
-        <Heading size="l">Money</Heading>
-        <Stack spacing={2}>
-          <Text>${characterData.money}</Text>
-          <Button color="teal" onClick={handleOpenSearch}>Pay</Button>
-        </Stack>
-      </GridItem>
-      <GridItem rowSpan={3} colSpan={2} overflowY="scroll">
-        <Stack justify="start" spacing={2}>
-          {characterData.bio.map((p) => (
-            <Text>{p}</Text>
-          ))}
-        </Stack>
-      </GridItem>
-      <GridItem rowSpan={1} colSpan={1} bg="white" />
-      <GridItem rowSpan={1} colSpan={1} bg="white" />
-      <GridItem rowSpan={1} colSpan={3} bg="tomato" />
-    </Grid>
+    <>
+      {characterData.name ? (
+        <Grid
+          h="80vh"
+          w="80vw"
+          templateRows="repeat(5, 1fr)"
+          templateColumns="repeat(3, 1fr)"
+          gap={3}
+        >
+          <GridItem rowSpan={1} colSpan={3}>
+            <Stack textAlign="center">
+              <Heading>{characterData.name}</Heading>
+              <Stack direction={'row'} spacing={2} justify="space-between">
+                <Text>Secret: {characterData.secret}</Text>
+                <Text>Clue: {characterData.clue}</Text>
+              </Stack>
+            </Stack>
+          </GridItem>
+          <GridItem rowSpan={1} colSpan={1}>
+            <Heading size="l">Money</Heading>
+            <Stack spacing={2}>
+              <Text>${characterData.money}</Text>
+              <Button color="teal" onClick={handleOpenSearch}>
+                Pay
+              </Button>
+            </Stack>
+          </GridItem>
+          <GridItem rowSpan={3} colSpan={2} overflowY="scroll">
+            <Stack justify="start" spacing={2}>
+              {characterData.bio.map((p) => (
+                <Text>{p}</Text>
+              ))}
+            </Stack>
+          </GridItem>
+          <GridItem rowSpan={1} colSpan={1} bg="white" />
+          <GridItem rowSpan={1} colSpan={1} bg="white" />
+          <GridItem rowSpan={1} colSpan={3} bg="tomato" />
+        </Grid>
+      ) : (
+        <> </>
+      )}
+    </>
   );
 }
 
