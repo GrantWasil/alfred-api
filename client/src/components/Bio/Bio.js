8224fd1d0e5a86727b2d8c127d033699835bb527
Grant Wasil <dev@grantwasil.com>
2021-02-02 21:12:57 -0700

    feat: fixed data loading on bio

--- client/src/components/Bio/Bio.js
+++ client/src/components/Bio/Bio.js
@@ -5,25 +5,31 @@ import { Stack, Heading, Text, Flex } from '@chakra-ui/react';
 function Bio(props) {
   const { characterData } = props;
   return (
-    <Stack spacing={3} textAlign="center">
-      <Heading>{characterData.name}</Heading>
-      <Flex>
-        <Heading size="l">Your secret: </Heading>
-        <Text>{characterData.secret}</Text>
-      </Flex>
-      <Flex>
-        <Heading size="l">Your clue: </Heading>
-        <Text>{characterData.clue}</Text>
-      </Flex>
-      <Heading size="l">Starting tips:</Heading>
-      {characterData.tips.map((t) => {
-        return <Text>{t}</Text>;
-      })}
-      <Heading size="l">YourbBio:</Heading>
-      {characterData.bio.map((p) => {
-        return <Text>{p}</Text>;
-      })}
-    </Stack>
+    <>
+      {characterData.name ? (
+        <Stack spacing={3} textAlign="center">
+          <Heading>{characterData.name}</Heading>
+          <Flex>
+            <Heading size="l">Your secret: </Heading>
+            <Text>{characterData.secret}</Text>
+          </Flex>
+          <Flex>
+            <Heading size="l">Your clue: </Heading>
+            <Text>{characterData.clue}</Text>
+          </Flex>
+          <Heading size="l">Starting tips:</Heading>
+          {characterData.tips.map((t) => {
+            return <Text>{t}</Text>;
+          })}
+          <Heading size="l">YourbBio:</Heading>
+          {characterData.bio.map((p) => {
+            return <Text>{p}</Text>;
+          })}
+        </Stack>
+      ) : (
+        <> </>
+      )}
+    </>
   );
 }
 
